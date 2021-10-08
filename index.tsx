8d685bb42770015b02385231cde353fcacbaf9f3
SaiMV <saimv09@gmail.com>
2021-10-09 03:12:05 +0530

    1 file modified

--- index.tsx
+++ index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import store from './store'
 
 import Hello from './containers/Hello';
 import { Provider } from 'react-redux';
